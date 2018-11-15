fe4c62de7b15f55cbf7e06d5daf0c6f7833ea196
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2018-11-15 11:56:23 +0100

    indexedDBUtils.js: fix indexedDB calls test/masq.test.js: fix calls to masq.init test: add indexedDBUtils test package-lock.json: update file

--- src/indexedDBUtils.js
+++ src/indexedDBUtils.js
@@ -1,10 +1,10 @@
 module.exports.dbExists = function (dbName) {
   return new Promise((resolve, reject) => {
-    let req = global.indexedDB.open(dbName, false)
+    let req = window.indexedDB.open(dbName)
     let existed = true
     req.onsuccess = () => {
       req.result.close()
-      if (!existed) { global.indexedDB.deleteDatabase(dbName) }
+      if (!existed) { window.indexedDB.deleteDatabase(dbName) }
       resolve(existed)
     }
     req.onupgradeneeded = () => {
