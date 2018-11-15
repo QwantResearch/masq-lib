dafc14cf4aa9b269de1dce1ca0a643893a36d3dc
Levent DEMIR <levent.demir01@gmail.com>
2018-11-15 11:47:57 +0100

    check masq-profiles and data hyperdb at initialisation (#5)
    * check masq-profiles and data hyperdb at initialisation
    * move isCreated function to new module
    * index.js: invert isCreated condition
    * test/masq.test.js: mock dbExists function
    * test/masq.test.js: remove unused function
    * package.json: put the eslint back
    * index.js: add missing await
    * fix dbExists naming
    * indexedDBUtils: adding error management
    * index.js: remove unnecessary use of path.join
    * index.js: minor reformat: move require

---
+++ src/indexedDBUtils.js
@@ -0,0 +1,17 @@
+module.exports.dbExists = function (dbName) {
+  return new Promise((resolve, reject) => {
+    let req = global.indexedDB.open(dbName, false)
+    let existed = true
+    req.onsuccess = () => {
+      req.result.close()
+      if (!existed) { global.indexedDB.deleteDatabase(dbName) }
+      resolve(existed)
+    }
+    req.onupgradeneeded = () => {
+      existed = false
+    }
+    req.onerror = (err) => {
+      reject(err)
+    }
+  })
+}
