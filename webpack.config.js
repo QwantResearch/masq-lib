edde8d843c588175c8aba9118c26db624db44dbf
Sujeeban Thuraisamy <s.thuraisamy@qwant.com>
2019-01-29 18:14:22 +0100

    tmp

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './babelOut/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'masq.js',
